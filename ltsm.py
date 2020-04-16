1690df47ee1d4e6c05a36cfbb53d99e6731e64a2
ndhpro <ndhpro.psa@gmail.com>
2020-04-16 16:17:05 +0700

    classify

---
+++ ltsm.py
@@ -0,0 +1,37 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import sys
+from pathlib import Path
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.model_selection import GridSearchCV, train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+
+
+X = np.load('corpus.npy')
+labels = np.load('labels.npy')
+vocab_size = 12910
+
+model = Sequential()
+model.add(Embedding(input_dim=vocab_size,
+                    output_dim=32, input_length=X.shape[1]))
+model.add(SpatialDropout1D(0.4))
+model.add(LSTM(units=64, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, labels, test_size=0.2, random_state=42)
+
+
+batch_size = 128
+model.fit(X_train, y_train, epochs=10, batch_size=batch_size, verbose=2, validation_split=0.25)
+
+score, acc = model.evaluate(X_test, y_test, verbose=2, batch_size=batch_size)
+print("score: %.2f" % (score))
+print("acc: %.2f" % (acc))
---
+++ ml.py
@@ -0,0 +1,107 @@
+import os
+import sys
+from pathlib import Path
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.feature_selection import SelectFromModel
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC, SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+
+# Preparing data
+root_path = Path('data')
+folders = ['bashlite/', 'mirai/', 'others/', 'benign/']
+corpus = list()
+labels = list()
+
+for folder in folders:
+    for _, _, files in os.walk(root_path/folder):
+        for name in files:
+            try:
+                with open(root_path/folder/name, 'r') as f:
+                    doc = f.read()
+                corpus.append(doc)
+                if 'benign' in folder:
+                    labels.append(1)
+                else:
+                    labels.append(0)
+            except Exception as e:
+                print(e)
+
+corpus, index = np.unique(corpus, axis=0, return_index=True)
+labels = np.array(labels)[index]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    corpus, labels, test_size=0.3, random_state=42)
+
+# Vectorizer
+vec = CountVectorizer()
+X_train = vec.fit_transform(X_train)
+X_test = vec.transform(X_test)
+print(X_train.shape, X_test.shape)
+
+# Feature selection
+model = SelectFromModel(
+    LinearSVC(penalty="l1", dual=False, random_state=42).fit(X_train, y_train), prefit=True)
+X_train = model.transform(X_train).A
+X_test = model.transform(X_test).A
+
+# Normalizing data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+print(X_train.shape, X_test.shape)
+
+# Training
+names = ['Logistic Regression', 'SVM', 'Decision Tree',
+         'kNN', 'Naive Bayes', 'Random Forest']
+models = [
+    LogisticRegression(random_state=42, n_jobs=-1),
+    SVC(random_state=42),
+    DecisionTreeClassifier(random_state=42),
+    KNeighborsClassifier(n_jobs=-1),
+    GaussianNB(),
+    RandomForestClassifier(random_state=42, n_jobs=-1)
+]
+hyperparams = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
+]
+colors = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink', 'gray']
+
+for name, model, hyper, color in zip(names, models, hyperparams, colors):
+    clf = GridSearchCV(model, param_grid=hyper, cv=5, n_jobs=-1) 
+    print(name)
+
+    clf.fit(X_train, y_train)
+    y_hat = clf.predict(X_test)
+
+    print(metrics.classification_report(y_test, y_hat))
+    print(metrics.confusion_matrix(y_test, y_hat))
+    print()
+
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_hat)
+    auc = metrics.roc_auc_score(y_test, y_hat)
+    plt.plot(fpr, tpr, color=color, label='%s (AUC = %0.3f)' % (name, auc))
+
+# Drawing ROC curve
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.plot([0, 1], [0, 1], 'r--')
+plt.xlabel('1-Specificity(False Positive Rate)')
+plt.ylabel('Sensitivity(True Positive Rate)')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
