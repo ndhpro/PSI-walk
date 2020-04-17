cf52fa8077d40e203abc6a4eda607b7e377b9124
ndhpro <ndhpro.psa@gmail.com>
2020-04-17 14:44:36 +0700

    big update

--- lstm.py
+++ lstm.py
@@ -1,38 +1,58 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import sys
-from pathlib import Path
-import numpy as np
-from matplotlib import pyplot as plt
-from sklearn.model_selection import GridSearchCV, train_test_split
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
 from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from keras.models import Sequential
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from sklearn.model_selection import GridSearchCV, train_test_split
+from matplotlib import pyplot as plt
+import numpy as np
+from pathlib import Path
 
 
-X = np.load('corpus.npy')
-labels = np.load('labels.npy')
-print('Corpus size: ', X.shape)
-vocab_size = 12910
+# Loading corpus
+corpus = list()
+labels = list()
+with open(Path('corpus/malware.txt'), 'r') as f:
+    lines = f.readlines()
+corpus.extend([data[:-1] for data in lines])
+labels.extend([0] * len(lines))
+
+with open(Path('corpus/benign.txt'), 'r') as f:
+    lines = f.readlines()
+corpus.extend([data[:-1] for data in lines])
+labels.extend([1] * len(lines))
+
+tokenizer = Tokenizer(filters='')
+tokenizer.fit_on_texts(corpus)
+X = tokenizer.texts_to_sequences(corpus)
+X = pad_sequences(X, maxlen=100)
+vocab_size = len(tokenizer.word_index) + 1
+print(vocab_size)
+unique_word = 0
+for k in tokenizer.word_docs:
+    if tokenizer.word_docs[k] == 1:
+        unique_word += 1
+print(unique_word)
 
 model = Sequential()
 model.add(Embedding(input_dim=vocab_size,
-                    output_dim=32, input_length=X.shape[1]))
+                    output_dim=16, input_length=X.shape[1]))
 model.add(SpatialDropout1D(0.4))
-model.add(LSTM(units=64, dropout=0.2, recurrent_dropout=0.2))
+model.add(LSTM(units=16, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy',
               optimizer='adam', metrics=['accuracy'])
-print(model.summary())
+# print(model.summary())
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, labels, test_size=0.2, random_state=42)
-
+    X, labels, test_size=0.15, random_state=42)
+print(X_train.shape, X_test.shape)
 
 batch_size = 128
-model.fit(X_train, y_train, epochs=10, batch_size=batch_size, verbose=2, validation_split=0.25)
+model.fit(X_train, y_train, epochs=10, batch_size=batch_size,
+          verbose=2, validation_split=3/17)
 
 score, acc = model.evaluate(X_test, y_test, verbose=2, batch_size=batch_size)
-print("score: %.2f" % (score))
-print("acc: %.2f" % (acc))
+print("test loss: %.4f" % (score))
+print("accuracy: %.4f" % (acc))
--- ml.py
+++ ml.py
@@ -1,54 +1,31 @@
-import os
-import sys
-from pathlib import Path
-import numpy as np
-from matplotlib import pyplot as plt
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.feature_selection import SelectFromModel
-from sklearn.preprocessing import StandardScaler, Normalizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import LinearSVC, SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC, SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler, Normalizer
+from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from matplotlib import pyplot as plt
+import numpy as np
+from pathlib import Path
 
 
-# Preparing data
-root_path = Path('data')
-folders = ['bashlite/', 'mirai/', 'others/', 'benign/']
+# Loading corpus
 corpus = list()
 labels = list()
+with open(Path('corpus/malware.txt'), 'r') as f:
+    lines = f.readlines()
+corpus.extend([data[:-1] for data in lines])
+labels.extend([0] * len(lines))
 
-for folder in folders:
-    for _, _, files in os.walk(root_path/folder):
-        for name in files:
-            try:
-                with open(root_path/folder/name, 'r') as f:
-                    doc = f.read().replace('\n', ' ')[:-1]
-                corpus.append(doc)
-                if 'benign' in folder:
-                    labels.append(1)
-                else:
-                    labels.append(0)
-            except Exception as e:
-                print(e)
-
-corpus, index = np.unique(corpus, axis=0, return_index=True)
-labels = np.array(labels)[index]
-
-# Saving for analysis
-with open('corpus_malware.txt', 'w') as f:
-    print(len(corpus[labels==0]))
-    for i in corpus[labels==0]:
-        f.write(str(i) + '\n')
-
-with open('corpus_benign.txt', 'w') as f:
-    print(len(corpus[labels==1]))
-    for i in corpus[labels==1]:
-        f.write(str(i) + '\n')
+with open(Path('corpus/benign.txt'), 'r') as f:
+    lines = f.readlines()
+corpus.extend([data[:-1] for data in lines])
+labels.extend([1] * len(lines))
 
 X_train, X_test, y_train, y_test = train_test_split(
     corpus, labels, test_size=0.3, random_state=42)
@@ -64,8 +41,6 @@ model = SelectFromModel(
     LinearSVC(penalty="l1", dual=False, random_state=42).fit(X_train, y_train), prefit=True)
 X_train = model.transform(X_train).A
 X_test = model.transform(X_test).A
-
-np.savez('data_ml', X_train, X_test, y_train, y_test)
 
 # Normalizing data
 scaler = StandardScaler()
@@ -95,10 +70,11 @@ hyperparams = [
 colors = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink', 'gray']
 
 for name, model, hyper, color in zip(names, models, hyperparams, colors):
-    clf = GridSearchCV(model, param_grid=hyper, cv=5, n_jobs=-1) 
+    clf = GridSearchCV(model, param_grid=hyper, cv=5, n_jobs=-1)
     print(name)
 
     clf.fit(X_train, y_train)
+
     y_hat = clf.predict(X_test)
 
     print(metrics.classification_report(y_test, y_hat))
@@ -108,11 +84,6 @@ for name, model, hyper, color in zip(names, models, hyperparams, colors):
     fpr, tpr, thresholds = metrics.roc_curve(y_test, y_hat)
     auc = metrics.roc_auc_score(y_test, y_hat)
     plt.plot(fpr, tpr, color=color, label='%s (AUC = %0.3f)' % (name, auc))
-
-    # y_hat = clf.predict(X_train)
-    # print(metrics.classification_report(y_train, y_hat))
-    # print(metrics.confusion_matrix(y_train, y_hat))
-    # print()
 
 # Drawing ROC curve
 plt.xlim([0.0, 1.0])
