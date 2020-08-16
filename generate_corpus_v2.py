3ce6b16d16c98e5395d963bdbd076b9e5ea32380
ndhpro <ndhpro.psa@gmail.com>
2020-08-16 19:37:41 +0700

    Add: new data

---
+++ generate_corpus_v2.py
@@ -0,0 +1,81 @@
+import os
+from pathlib import Path
+import numpy as np
+import random
+from sklearn.model_selection import train_test_split
+
+
+def random_augment(words):
+    new_words = []
+    pre_word = ''
+    for word in words:
+        new_words.append(word)
+        r = random.uniform(0, 3)
+        if word == pre_word:
+            new_words.extend([word]*int(r))
+        pre_word = word
+    new_words = ' '.join(new_words)
+    return new_words
+
+
+# Preparing data
+root_path = Path('datav2')
+folders = ['malware/', 'benign/']
+corpus = list()
+labels = list()
+
+for folder in folders:
+    for _, _, files in os.walk(root_path/folder):
+        for name in files:
+            try:
+                with open(root_path/folder/name, 'r') as f:
+                    doc = f.read().replace('\n', ' ')[:-1]
+                words = doc.split(' ')
+                enc = True
+                for word in words:
+                    if not 'sub' in word:
+                        enc = False
+                        break
+                if enc:
+                    continue
+                corpus.append(doc)
+                if 'benign' in folder:
+                    labels.append(0)
+                else:
+                    labels.append(1)
+            except Exception as e:
+                print(e)
+
+corpus, index = np.unique(corpus, axis=0, return_index=True)
+labels = np.array(labels)[index]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    corpus, labels, test_size=0.3, random_state=2020)
+
+# X_aug, y_aug = [], []
+# for i in range(len(X_train)):
+#     aug = 1
+#     doc = X_train[i]
+#     # Data augmentation
+#     doc = doc.split(' ')
+#     while len(doc) > 4 and aug < 4:
+#         new_doc = random_augment(doc)
+#         if new_doc != doc:
+#             aug += 1
+#             X_aug.append(new_doc)
+#             y_aug.append(y_train[i])
+# X_train = list(X_train)
+# X_train.extend(X_aug)
+# y_train = list(y_train)
+# y_train.extend(y_aug)
+
+# Saving for analysis
+with open('corpus/train_v2.txt', 'w') as f:
+    for i in range(len(X_train)):
+        f.write(str(X_train[i]) + ' ' + str(y_train[i]) + '\n')
+
+with open('corpus/test_v2.txt', 'w') as f:
+    for i in range(len(X_test)):
+        f.write(str(X_test[i]) + ' ' + str(y_test[i]) + '\n')
+
+print(len(X_train), len(X_test))
---
+++ lstm_v2.py
@@ -0,0 +1,132 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import itertools
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+from sklearn import metrics
+from matplotlib import pyplot as plt
+import numpy as np
+from pathlib import Path
+
+# Loading corpus
+X_train, X_test = list(), list()
+y_train, y_test = list(), list()
+with open(Path('corpus/train_v2.txt'), 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        X_train.append(line[:-3])
+        y_train.append(int(line[-2]))
+
+with open(Path('corpus/test_v2.txt'), 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        X_test.append(line[:-3])
+        y_test.append(int(line[-2]))
+
+print(len(X_train), len(X_test))
+
+tokenizer = Tokenizer(filters='')
+tokenizer.fit_on_texts(X_train)
+
+X_train = tokenizer.texts_to_sequences(X_train)
+X_train = pad_sequences(X_train, maxlen=100)
+y_train = np.array(y_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+X_test = pad_sequences(X_test, maxlen=100)
+y_test = np.array(y_test)
+
+vocab_size = len(tokenizer.word_index) + 1
+print(f'Vocab size: {vocab_size}, Unique nodes:', end=' ')
+unique_word = 0
+for k in tokenizer.word_docs:
+    if tokenizer.word_docs[k] == 1:
+        unique_word += 1
+print(unique_word)
+
+model = Sequential()
+model.add(Embedding(input_dim=vocab_size,
+                    output_dim=16, input_length=X_train.shape[1]))
+model.add(SpatialDropout1D(0.4))
+model.add(LSTM(units=16, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
+# print(model.summary())
+
+batch_size = 512
+mc = ModelCheckpoint('output_v2/model.h5', save_best_only=True,
+                     save_weights_only=True)
+history = model.fit(X_train, y_train, epochs=100, batch_size=batch_size,
+                    verbose=2, validation_split=1/5,
+                    callbacks=[mc])
+
+model.load_weights('output_v2/model.h5')
+y_pred = model.predict(X_test, verbose=1, batch_size=batch_size)
+y_pred = [y >= 0.5 for y in y_pred]
+
+print(metrics.classification_report(y_test, y_pred, digits=4))
+print()
+
+# Plot the loss and accuracy curves for training and validation
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(history.history['loss'], color='b', label="Training loss")
+ax[0].plot(history.history['val_loss'], color='r',
+           label="validation loss", axes=ax[0])
+legend = ax[0].legend(loc='best', shadow=True)
+
+ax[1].plot(history.history['accuracy'], color='b', label="Training accuracy")
+ax[1].plot(history.history['val_accuracy'],
+           color='r', label="Validation accuracy")
+legend = ax[1].legend(loc='best', shadow=True)
+plt.savefig('output_v2/training_history.png')
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.get_cmap('Blues')):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+plot_confusion_matrix(metrics.confusion_matrix(y_test, y_pred), classes=[0, 1])
+plt.savefig('output_v2/confusion_matrix.png')
+
+# Drawing ROC curve
+plt.figure()
+fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
+auc = metrics.roc_auc_score(y_test, y_pred)
+plt.plot(fpr, tpr, color='blue', label='AUC = %0.4f' % (auc))
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.plot([0, 1], [0, 1], 'r--')
+plt.xlabel('1-Specificity(False Positive Rate)')
+plt.ylabel('Sensitivity(True Positive Rate)')
+plt.title('Receiver Operating Characteristic')
+plt.legend()
+plt.savefig('output_v2/roc.png')
